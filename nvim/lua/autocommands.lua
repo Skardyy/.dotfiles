fd9b1c55bb20c9ec62d2d225e5901f3650bf80b4
skardy <meronb1312@gmail.com>
2024-08-03 15:14:34 +0300

    forgot to add those

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -1,16 +1,10 @@
 -- Auto-format on save
-vim.api.nvim_create_augroup("AutoFormat", {})
-vim.api.nvim_create_autocmd(
-    "BufWritePost",
-    {
-        pattern = "*",
-        group = "AutoFormat",
-        callback = function()
-            vim.cmd("silent !black --quiet %")
-            vim.cmd("edit")
-        end,
-    }
-)
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
+})
 
 -- Return to last edit position when opening files
 vim.api.nvim_create_autocmd("BufReadPost", {
--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -4,12 +4,12 @@ local opts = { noremap = true, silent = true }
 -- Normal mode mappings
 ---------------------------------
 
-vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>q', ':q<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>h', ':noh<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>/', 'gcc', { noremap = false, silent = true } )
-vim.api.nvim_set_keymap('n', '<leader>n', ':cn<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>N', ':cp<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { noremap = true, silent = true, desc = 'Save' })
+vim.api.nvim_set_keymap('n', '<leader>q', ':q<CR>', { noremap = true, silent = true, desc = 'Quit' })
+vim.api.nvim_set_keymap('n', '<leader>h', ':noh<CR>', { noremap = true, silent = true, desc = 'No highlight' })
+vim.api.nvim_set_keymap('n', '<leader>/', 'gcc', { noremap = false, silent = true, desc = "Comment" } )
+vim.api.nvim_set_keymap('n', '<leader>n', ':cn<CR>', { noremap = true, silent = true, desc = 'Next error' })
+vim.api.nvim_set_keymap('n', '<leader>N', ':cp<CR>', { noremap = true, silent = true, desc = 'Previous error' })
 vim.api.nvim_set_keymap('n', '<C-j>', '<C-d>', opts)
 vim.api.nvim_set_keymap('n', '<C-k>', '<C-u>', opts)
 
@@ -17,6 +17,6 @@ vim.api.nvim_set_keymap('n', '<C-k>', '<C-u>', opts)
 -- Visual mode mapping
 ---------------------------------
 
-vim.api.nvim_set_keymap('v', '<leader>/', 'gc', { noremap = false, silent = true })
-vim.api.nvim_set_keymap('v', '<Tab>', '>gv', opts)
-vim.api.nvim_set_keymap('v', '<S-Tab>', '<gv', opts)
+vim.api.nvim_set_keymap('v', '<leader>/', 'gc', { noremap = false, silent = true, desc = "Comment" })
+vim.api.nvim_set_keymap('v', '<Tab>', '>', opts)
+vim.api.nvim_set_keymap('v', '<S-Tab>', '<', opts)
