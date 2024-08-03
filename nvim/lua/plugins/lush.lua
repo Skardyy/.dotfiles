3d841d708f4d5d82f7616f6758f9b6b547e299e9
skardy <meronb1312@gmail.com>
2024-08-03 19:22:25 +0300

    fixing massive issue ive put in toggle term lol

--- nvim/lua/plugins/lush.lua
+++ nvim/lua/plugins/lush.lua
@@ -1,3 +1,4 @@
 return {
   "rktjmp/lush.nvim",
+  { dir = "C:/Users/meron/Desktop/makurai.nvim", lazy = true },
 }
--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -2,9 +2,15 @@ return {
   "akinsho/toggleterm.nvim",
   config = function()
     require("toggleterm").setup({
-      open_mapping = [[<Leader>t]],
       direction = "float",
       shell = os.getenv("SHELL") or "pwsh",
     })
+    vim.api.nvim_set_keymap("n", "<Leader>t", "<cmd>ToggleTerm<CR>", { noremap = true, silent = true })
+    function _G.set_terminal_keymaps()
+      local opts = { noremap = true }
+      vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+    end
+
+    vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
   end,
 }
