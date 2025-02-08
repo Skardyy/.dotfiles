011a7f4fcb626c6ef4663ceb6be10ba1d18749ed
Meron Main PC <meronb1312@gmail.com>
2025-02-08 22:14:56 +0200

    adding neo-img completely

--- nvim/lua/plugins/neo-img.lua
+++ nvim/lua/plugins/neo-img.lua
@@ -1,7 +1,6 @@
--- if true then return {} end
 return {
-  -- "skardyy/neo-img",
-  dir = "~/Desktop/neo-img",
+  "skardyy/neo-img",
+  -- dir = "~/Desktop/neo-img",
   config = function()
     require('neo-img').setup({
     })
