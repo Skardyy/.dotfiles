0dc51728d957307bc0cef8efbd3a8ed7e8dd4ece
Meron Main PC <meronb1312@gmail.com>
2025-03-22 02:46:13 +0200

    adding transparent option to the colorscheme

--- editors/nvim/lua/plugins/colorscheme.lua
+++ editors/nvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,9 @@ return {
     "Skardyy/makurai-nvim",
     -- dir = "~/Desktop/makurai-nvim",
     config = function()
+      require "makurai".setup({
+        transparent = true
+      })
       -- vim.cmd.colorscheme("makurai")
     end,
   },
