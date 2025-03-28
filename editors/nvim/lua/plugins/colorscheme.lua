9a9b7319e29f85d5ad0300d669dcfb3989b8f323
Meron Main PC <meronb1312@gmail.com>
2025-03-29 02:28:10 +0300

    modifing to the new version of makurai theme

--- editors/nvim/lua/plugins/colorscheme.lua
+++ editors/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,12 @@
 return {
-  {
-    "Skardyy/makurai-nvim",
-    -- dir = "~/Desktop/makurai-nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require "makurai".setup({
-        transparent = true
-      })
-      -- vim.cmd.colorscheme("makurai")
-    end,
-  },
+  "Skardyy/makurai-nvim",
+  -- dir = "~/Desktop/makurai-nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require "makurai".setup({
+      transparent = true
+    })
+    -- vim.cmd.colorscheme("makurai")
+  end,
 }
