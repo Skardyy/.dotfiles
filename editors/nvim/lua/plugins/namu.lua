c8187bcf752f54aad9472dd481fd7d7c4bcbf302
Meron Main PC <meronb1312@gmail.com>
2025-03-29 18:49:56 +0300

    adding more config to namu

--- editors/nvim/lua/plugins/namu.lua
+++ editors/nvim/lua/plugins/namu.lua
@@ -1,14 +1,31 @@
 return {
   "bassamsdata/namu.nvim",
   config = function()
+    local allow_kinds = {
+      "Function",
+      "Method",
+      "Class",
+      -- "Module",
+      -- "Property",
+      -- "Variable",
+      -- "Constant",
+      "Enum",
+      "Interface",
+      -- "Field",
+      "Struct",
+    }
     require("namu").setup({
       namu_symbols = {
         options = {
           movement = {
-            next = "<Tab>"
+            next = { "<Tab>", "<C-n>", "<DOWN>" }
           },
           multiselect = {
             enabled = false
+          },
+          AllowKinds = {
+            default = allow_kinds,
+            go = allow_kinds
           }
         }
       },
