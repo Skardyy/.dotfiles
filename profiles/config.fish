8e90ef7ef6f810a4e1a46cc19e102486402642df
meron arch <meronbssn@gmail.com>
2025-05-31 00:05:54 +0300

    changing to rogue theme, for now

--- profiles/config.fish
+++ profiles/config.fish
@@ -7,17 +7,23 @@ if status is-interactive
   set -x ANDROID_HOME /home/meron/Android/Sdk/
   set -x PATH $ANDROID_HOME/tools $PATH
   set -x PATH $ANDROID_HOME/platform-tools $PATH
-  set -gx FZF_DEFAULT_OPTS "
-  --color=bg+:#1e2029,bg:#15161B,spinner:#FFEE99,hl:#FFEE99 \
-  --color=fg:#A6ACCD,header:#FF7733,info:#D2A6FF,pointer:#FFEE99 \
-  --color=marker:#95FB79,fg+:#FFFFFF,prompt:#82AAFF,hl+:#95FB79 \
-  --color=border:#25282E \
-  --height 40% --border=rounded --reverse --margin=1 --padding=1"
+  export FZF_DEFAULT_OPTS="
+  --color=fg:#d0d0d2,hl:#92c468
+  --color=fg+:#ffffff,bg+:#1E1F24,hl+:#92c468
+  --color=info:#94A3C5,prompt:#f7e254,pointer:#ffdb29
+  --color=marker:#FF875F,spinner:#94A3C5,header:#24292e
+  --color=border:#1E1F24,label:#8e8e90,query:#e8e8ea
+  --color=disabled:#24292e,preview-fg:#e8e8ea
+  --border=rounded
+  --height=40%
+  --layout=reverse
+  --info=inline
+  --margin=1
+  --padding=1"
 
   alias v='nvim'
   alias e='hyprctl dispatch exec dolphin'
   alias scb='wl-copy'
   alias c="fd --type d --hidden --exclude .git --exclude node_modules . ~ | fzf --preview 'exa --tree --color=always {}' --bind tab:down | read -l dir; and cd \$dir; and commandline -f repaint"
   bind \co 'c'
-  bind \cj 'v .'
 end
