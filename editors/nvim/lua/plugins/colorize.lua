da9038a6310566b0f4b5172fc3a607d80819df64
Meron Main PC <meronb1312@gmail.com>
2025-06-10 20:45:05 +0300

    - changing the colorize back to normal mode

--- editors/nvim/lua/plugins/colorize.lua
+++ editors/nvim/lua/plugins/colorize.lua
@@ -2,10 +2,5 @@ return {
   "catgoose/nvim-colorizer.lua",
   event = "BufReadPre",
   opts = {
-    user_default_options = {
-      virtualtext = " ",
-      mode = "virtualtext",
-      virtualtext_inline = true,
-    },
   }
 }
