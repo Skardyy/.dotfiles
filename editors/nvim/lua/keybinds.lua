a92d91aee23d3f871393daecfda6ca6fc4c2e97d
meron arch <meronbssn@gmail.com>
2025-07-19 02:21:54 +0300

    feat: yonking a gtk theme from someone

--- editors/nvim/lua/keybinds.lua
+++ editors/nvim/lua/keybinds.lua
@@ -36,7 +36,7 @@ end
 -- Normal mode mappings
 ---------------------------------
 
-vim.api.nvim_set_keymap("n", "<leader>1", ":lua require('conform').format({})<CR>",
+vim.api.nvim_set_keymap("n", "<leader>1", "gg=G``",
   { noremap = true, silent = true, desc = "Format Buffer" })
 vim.api.nvim_set_keymap("n", "<leader>w", ":silent! w!<CR>", { noremap = true, silent = true, desc = "Save" })
 vim.api.nvim_set_keymap("n", "<leader>h", ":noh<CR>", { noremap = true, silent = true, desc = "No highlight" })
