ab945e991bfa30b5e6abcc84c20d32df6b79335f
Meron Main PC <meronb1312@gmail.com>
2025-10-22 21:27:25 +0300

    fix(nvim): fixing regression on treesitter setup.

--- editors/nvim/lua/plugins/lsp/treesitter.lua
+++ editors/nvim/lua/plugins/lsp/treesitter.lua
@@ -1,9 +1,13 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-      indent = { enable = true },
-    }
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
