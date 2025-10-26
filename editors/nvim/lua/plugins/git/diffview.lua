558796d039b7b3bea61ed79a1632637f7be53564
Meron Main PC <meronb1312@gmail.com>
2025-10-26 09:03:18 +0200

    feat(nvim): made a shortcut of DiffViewOpen to Diff

--- editors/nvim/lua/plugins/git/diffview.lua
+++ editors/nvim/lua/plugins/git/diffview.lua
@@ -5,6 +5,9 @@ return {
     local map = vim.keymap.set
     local opts = { noremap = true, silent = true, desc = "" }
 
+    vim.api.nvim_create_user_command("Diff", function(o)
+      vim.cmd("DiffviewOpen " .. table.concat(o.fargs, " "))
+    end, { nargs = "*" })
     map("n", "<leader>do", "<cmd>DiffviewOpen<cr>", vim.tbl_extend("force", opts, { desc = "Open Diffview" }))
     map("n", "<leader>dc", "<cmd>DiffviewClose<cr>", vim.tbl_extend("force", opts, { desc = "Close Diffview" }))
     map("n", "<leader>dm", "<cmd>DiffviewOpen origin/main...HEAD<cr>",
