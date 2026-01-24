2fa9a6a4c4ce544e04c12376a14d010f769261f8
meron Arch <meronbssn@gmail.com>
2026-01-24 15:46:14 +0200

    feat(nvim): adding snippet support

--- editors/nvim/lua/plugins/lsp/completions.lua
+++ editors/nvim/lua/plugins/lsp/completions.lua
@@ -1,99 +1,115 @@
 return {
-  "hrsh7th/nvim-cmp",
-  event = { "InsertEnter", "CmdlineEnter" },
-  dependencies = {
-    'hrsh7th/cmp-cmdline',
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-buffer",
+  {
+    "L3MON4D3/LuaSnip",
+    version = "2.*",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+      local luasnip = require("luasnip")
+      vim.keymap.set({ "i", "s" }, "<Tab>", function()
+        if luasnip.jumpable(1) then
+          luasnip.jump(1)
+        end
+      end)
+    end
   },
-  config = function()
-    local cmp = require("cmp")
-    local icons = {
-      Text = "󰉿",
-      Method = "󰆧",
-      Function = "󰊕",
-      Constructor = "",
-      Field = "󰜢",
-      Variable = "󰀫",
-      Class = "󰠱",
-      Interface = "",
-      Module = "",
-      Property = "󰜢",
-      Unit = "󰑭",
-      Value = "󰎠",
-      Enum = "",
-      Keyword = "󰌋",
-      Snippet = "",
-      Color = "󰏘",
-      File = "󰈙",
-      Reference = "󰈇",
-      Folder = "󰉋",
-      EnumMember = "",
-      Constant = "󰏿",
-      Struct = "󰙅",
-      Event = "",
-      Operator = "󰆕",
-      TypeParameter = "",
-    }
-    cmp.setup({
-      window = {
+  {
+    "hrsh7th/nvim-cmp",
+    event = { "InsertEnter", "CmdlineEnter" },
+    dependencies = {
+      'hrsh7th/cmp-cmdline',
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local icons = {
+        Text = "󰉿",
+        Method = "󰆧",
+        Function = "󰊕",
+        Constructor = "",
+        Field = "󰜢",
+        Variable = "󰀫",
+        Class = "󰠱",
+        Interface = "",
+        Module = "",
+        Property = "󰜢",
+        Unit = "󰑭",
+        Value = "󰎠",
+        Enum = "",
+        Keyword = "󰌋",
+        Snippet = "",
+        Color = "󰏘",
+        File = "󰈙",
+        Reference = "󰈇",
+        Folder = "󰉋",
+        EnumMember = "",
+        Constant = "󰏿",
+        Struct = "󰙅",
+        Event = "",
+        Operator = "󰆕",
+        TypeParameter = "",
+      }
+      cmp.setup({
+        window = {
+          completion = {
+            border = "rounded",
+            winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+            col_offset = -3,
+            side_padding = 0,
+            max_height = 10,
+          },
+          documentation = {
+            border = "rounded",
+            winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder",
+            max_width = 80,
+            max_height = 10,
+          },
+        },
+        formatting = {
+          fields = { "menu", "abbr", "kind" },
+          format = function(_, vim_item)
+            local kind_hl_group = "CmpItemKind" .. vim_item.kind
+            vim_item.menu = string.format(" %s ", icons[vim_item.kind])
+            vim_item.menu_hl_group = kind_hl_group
+            return vim_item
+          end,
+        },
         completion = {
-          border = "rounded",
-          winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
-          col_offset = -3,
-          side_padding = 0,
-          max_height = 10,
+          completeopt = 'menu,menuone,noinsert',
         },
-        documentation = {
-          border = "rounded",
-          winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder",
-          max_width = 80,
-          max_height = 10,
+        experimental = {
+          ghost_text = false,
         },
-      },
-      formatting = {
-        fields = { "menu", "abbr", "kind" },
-        format = function(_, vim_item)
-          local kind_hl_group = "CmpItemKind" .. vim_item.kind
-          vim_item.menu = string.format(" %s ", icons[vim_item.kind])
-          vim_item.menu_hl_group = kind_hl_group
-          return vim_item
-        end,
-      },
-      completion = {
-        completeopt = 'menu,menuone,noinsert',
-      },
-      experimental = {
-        ghost_text = false,
-      },
-      preselect = cmp.PreselectMode.None,
-      sorting = {
-        priority_weight = 1.0,
-        comparators = {
-          cmp.config.compare.locality,
-          cmp.config.compare.recently_used,
-          cmp.config.compare.score,
-          cmp.config.compare.offset,
-          cmp.config.compare.order,
+        preselect = cmp.PreselectMode.None,
+        sorting = {
+          priority_weight = 1.0,
+          comparators = {
+            cmp.config.compare.locality,
+            cmp.config.compare.recently_used,
+            cmp.config.compare.score,
+            cmp.config.compare.offset,
+            cmp.config.compare.order,
+          },
         },
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-        ["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-        ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-        ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-        ["<CR>"] = cmp.mapping(cmp.mapping.confirm({ select = false }), { "i", "c" }),
-        ["<C-K>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-        ["<C-J>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-      }),
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "path" },
-      }, {
-        { name = "buffer" },
-      }),
-    })
-  end,
+        mapping = {
+          ["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<C-K>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<C-J>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<CR>"] = cmp.mapping(cmp.mapping.confirm({ select = false }), { "i", "c" }),
+          ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+        },
+        sources = cmp.config.sources({
+          { name = "luasnip" },
+          { name = "nvim_lsp" },
+          { name = "path" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
+  }
 }
