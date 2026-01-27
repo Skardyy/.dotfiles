7080d8dbe6dcae4acaf10da7cfaefde23477c59b
meron <meronb@cloud-burst.net>
2026-01-27 13:13:11 +0200

    chore(noop): idk

--- profiles/config.fish
+++ profiles/config.fish
@@ -1,28 +1,29 @@
 if status is-interactive
-  export PATH="$HOME/.local/bin:$PATH"
-  export PATH="$HOME/.cargo/bin:$PATH"
-  export PATH="/opt/homebrew:$PATH"
-  export PATH="/opt/homebrew/bin:$PATH"
-  export PROFILE="$HOME/.config/fish/config.fish"
-  export CC=clang
-  export CXX=clang++
-  set -x ANDROID_HOME /home/meron/Android/Sdk/
-  # set -x ANDROID_HOME ~/Library/Android/sdk
-  set -x PATH $ANDROID_HOME/tools $PATH
-  set -x PATH $ANDROID_HOME/platform-tools $PATH
+    source ~/.config/fish/tmp.fish
+    export PATH="$HOME/.local/bin:$PATH"
+    export PATH="$HOME/.cargo/bin:$PATH"
+    export PATH="/opt/homebrew:$PATH"
+    export PATH="/opt/homebrew/bin:$PATH"
+    export PROFILE="$HOME/.config/fish/config.fish"
+    export CC=clang
+    export CXX=clang++
+    set -x ANDROID_HOME /home/meron/Android/Sdk/
+    # set -x ANDROID_HOME ~/Library/Android/sdk
+    set -x PATH $ANDROID_HOME/tools $PATH
+    set -x PATH $ANDROID_HOME/platform-tools $PATH
 
-  alias v='nvim'
-  alias o='mcat'
-  alias pine="PROTONPATH=GE-Proton umu-run"
-  alias ls="eza"
-  function e
-      set -l target "."
-      if test (count $argv) -gt 0
-          set target "$argv[1]"
-      end
+    alias v='nvim'
+    alias o='mcat'
+    alias pine="PROTONPATH=GE-Proton umu-run"
+    alias ls="eza"
+    function e
+        set -l target "."
+        if test (count $argv) -gt 0
+            set target "$argv[1]"
+        end
 
-      set target (realpath "$target")
-      hyprctl dispatch exec nemo "$target"
-  end
-  alias scb='wl-copy'
+        set target (realpath "$target")
+        hyprctl dispatch exec nemo "$target"
+    end
+    alias scb='wl-copy'
 end
