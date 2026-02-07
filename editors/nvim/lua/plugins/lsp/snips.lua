5e78a727abe7fa4c6c0ae1df1442e3403f507237
meron@arch <meronbssn@gmail.com>
2026-02-07 18:19:54 +0200

    feat(nvim): making the postfix snips smarter

--- editors/nvim/lua/plugins/lsp/snips.lua
+++ editors/nvim/lua/plugins/lsp/snips.lua
@@ -4,27 +4,52 @@ return {
   event = "InsertEnter",
   config = function()
     local ls = require("luasnip")
-    local postfix = require("luasnip.extras.postfix").postfix
+    local p = require("luasnip.extras.postfix").postfix
     local f = ls.function_node
+    local i = ls.insert_node
+
+    local function wrap_logic(before, after, match)
+      local result = before .. match .. after
+
+      if result:find("\n") then
+        local lines = {}
+        for line in result:gmatch("[^\n]+") do
+          table.insert(lines, line)
+        end
+        return lines
+      end
+
+      return result
+    end
 
     local function wrap(before, after)
       return f(function(_, parent)
         local match = parent.snippet.env.POSTFIX_MATCH
-        local result = before .. match .. after
-
-        if result:find("\n") then
-          local lines = {}
-          for line in result:gmatch("[^\n]+") do
-            table.insert(lines, line)
-          end
-          return lines
-        end
-
-        return result
+        return wrap_logic(before, after, match)
       end, {})
     end
 
-    local function smart_wrap(pre, optional_pre, optional_post, post)
+    local function smart_wrap(before, after)
+      return {
+        f(function(_, parent)
+          local match = parent.snippet.env.POSTFIX_MATCH
+          if match == "" then
+            return before
+          end
+          return wrap_logic(before, after, match)
+        end, {}),
+        i(1),
+        f(function(_, parent)
+          local match = parent.snippet.env.POSTFIX_MATCH
+          if match == "" then
+            return after
+          end
+          return ""
+        end, {}),
+      }
+    end
+
+    local function git_commit_wrap(pre, optional_pre, optional_post, post)
       return f(function(_, parent)
         local match = parent.snippet.env.POSTFIX_MATCH
         if match == "" then
@@ -35,46 +60,50 @@ return {
       end, {})
     end
 
+    local function postfix(trig, nodes)
+      return p({ trig = trig, match_pattern = ".*" }, nodes)
+    end
+
     -- RUST
     ls.add_snippets("rust", {
-      postfix(".rc", wrap("Rc::new(", ");")),
-      postfix(".arc", wrap("Arc::new(", ");")),
-      postfix(".mutex", wrap("Mutex::new(", ");")),
-      postfix(".rwlock", wrap("RwLock::new(", ");")),
-      postfix(".println", wrap('println!("{:?}", ', ");")),
-      postfix(".eprintln", wrap('eprintln!("{:?}", ', ");")),
-      postfix(".format", wrap('format!("{}", ', ");")),
+      postfix(".rc", smart_wrap("Rc::new(", ");")),
+      postfix(".arc", smart_wrap("Arc::new(", ");")),
+      postfix(".mutex", smart_wrap("Mutex::new(", ");")),
+      postfix(".rwlock", smart_wrap("RwLock::new(", ");")),
+      postfix(".println", smart_wrap('println!("{:?}", ', ");")),
+      postfix(".eprintln", smart_wrap('eprintln!("{:?}", ', ");")),
+      postfix(".format", smart_wrap('format!("{}", ', ");")),
     })
 
     -- MARKDOWN
     ls.add_snippets("markdown", {
-      postfix(".bold", wrap("**", "**")),
-      postfix(".italic", wrap("*", "*")),
-      postfix(".code", wrap("`", "`")),
-      postfix(".strike", wrap("~~", "~~")),
+      postfix(".bold", smart_wrap("**", "**")),
+      postfix(".italic", smart_wrap("*", "*")),
+      postfix(".code", smart_wrap("`", "`")),
+      postfix(".strike", smart_wrap("~~", "~~")),
 
-      postfix({ trig = ".note", match_pattern = ".*" }, wrap("> [!NOTE]\n> ", "")),
-      postfix({ trig = ".tip", match_pattern = ".*" }, wrap("> [!TIP]\n> ", "")),
-      postfix({ trig = ".important", match_pattern = ".*" }, wrap("> [!IMPORTANT]\n> ", "")),
-      postfix({ trig = ".warning", match_pattern = ".*" }, wrap("> [!WARNING]\n> ", "")),
-      postfix({ trig = ".caution", match_pattern = ".*" }, wrap("> [!CAUTION]\n> ", "")),
+      postfix(".note", wrap("> [!NOTE]\n> ", "")),
+      postfix(".tip", wrap("> [!TIP]\n> ", "")),
+      postfix(".important", wrap("> [!IMPORTANT]\n> ", "")),
+      postfix(".warning", wrap("> [!WARNING]\n> ", "")),
+      postfix(".caution", wrap("> [!CAUTION]\n> ", "")),
     })
 
     -- 2. GITCOMMIT
     ls.add_snippets("gitcommit", {
-      postfix({ trig = ".feat", match_pattern = ".*" }, smart_wrap("feat", "(", ")", ": ")),
-      postfix({ trig = ".fix", match_pattern = ".*" }, smart_wrap("fix", "(", ")", ": ")),
-      postfix({ trig = ".chore", match_pattern = ".*" }, smart_wrap("chore", "(", ")", ": ")),
-      postfix({ trig = ".doc", match_pattern = ".*" }, smart_wrap("docs", "(", ")", ": ")),
-      postfix({ trig = ".refactor", match_pattern = ".*" }, smart_wrap("refactor", "(", ")", ": ")),
+      postfix(".feat", git_commit_wrap("feat", "(", ")", ": ")),
+      postfix(".fix", git_commit_wrap("fix", "(", ")", ": ")),
+      postfix(".chore", git_commit_wrap("chore", "(", ")", ": ")),
+      postfix(".doc", git_commit_wrap("docs", "(", ")", ": ")),
+      postfix(".refactor", git_commit_wrap("refactor", "(", ")", ": ")),
     })
     ls.filetype_extend("gitcommit", { "markdown" })
 
     -- TYPESCRIPT AND FRIENDS
     ls.add_snippets("javascript", {
-      postfix(".log", wrap("console.log(", ")")),
-      postfix(".await", wrap("await ", "")),
-      postfix(".json", wrap("JSON.stringify(", ", null, 2)")),
+      postfix(".log", smart_wrap("console.log(", ")")),
+      postfix(".await", smart_wrap("await ", "")),
+      postfix(".json", smart_wrap("JSON.stringify(", ", null, 2)")),
     })
     ls.filetype_extend("typescript", { "javascript" })
     ls.filetype_extend("javascriptreact", { "javascript" })
@@ -82,8 +111,8 @@ return {
 
     -- PYTHON
     ls.add_snippets("python", {
-      postfix(".print", wrap("print(", ")")),
-      postfix(".len", wrap("len(", ")")),
+      postfix(".print", smart_wrap("print(", ")")),
+      postfix(".len", smart_wrap("len(", ")")),
     })
   end,
 }
