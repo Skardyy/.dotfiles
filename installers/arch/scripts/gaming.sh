1ddba1eea4ae5ec8264f7ea37d7a58ea28b95841
meron artix <meronbssn@gmail.com>
2026-04-11 02:21:39 +0300

    chore: removing unused installers

--- installers/arch/scripts/gaming.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo -e "\n=== Gaming Setup ==="
-sudo pacman -S --needed --noconfirm \
-  steam \
-  discord \
-  gamemode lib32-gamemode \
-  umu-launcher
-
-sudo usermod -aG gamemode $USER
--- installers/artix/nvidia.sh
+++ installers/artix/nvidia.sh
@@ -3,13 +3,14 @@
 sudo pacman -S --noconfirm --needed nvidia-dkms nvidia-utils lib32-nvidia-utils
 
 echo "============================================================="
-echo "Edit /etc/mkinitcpio.conf"
-echo "add to MODULES=(... nvidia_modeset nvidia_uvm nvidia_drm)"
-echo "============================================================="
 echo "Edit /etc/default/grub"
 echo "add to GRUB_CMDLINE_LINUX_DEFAULT='... nvidia_drm.modeset=1'"
-echo "============================================================="
 echo "Call"
 echo "sudo grub-mkconfig -o /boot/grub/grub.cfg"
+echo "============================================================="
+echo "Only if you need early kms"
+echo "Edit /etc/mkinitcpio.conf"
+echo "add to MODULES=(... nvidia nvidia_modeset nvidia_uvm nvidia_drm)"
+echo "Call"
 echo "sudo mkinitcpio -P"
 echo "============================================================="
