a1c57de4387b98cdd7052a380fb767683de0d3ce
nixos-meron <meronbssn@gmail.com>
2026-05-15 15:40:53 +0300

    feat: chaning to use lix

--- flake.nix
+++ flake.nix
@@ -9,6 +9,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     dms = {
       url = "github:AvengeMedia/DankMaterialShell/stable";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -22,7 +27,7 @@
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
   };
 
-  outputs = { self, nixpkgs, home-manager, dms, nix-darwin, nix-homebrew, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, dms, nix-darwin, nix-homebrew, lix-module, ... }@inputs: {
     nixosConfigurations = {
       nixos-meron = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -30,6 +35,7 @@
         modules = [
           ./hosts/nixos-meron
           home-manager.nixosModules.home-manager
+          lix-module.nixosModules.lixFromNixpkgs
         ];
       };
     };
