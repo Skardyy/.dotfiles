6c3d34733399536e1acbed54cb9008b536e0e305
nixos-meron <meronbssn@gmail.com>
2026-08-08 22:58:49 +0300

    chore(noctalia): saving v5 settings
    not going to use them now, still perfer v4, but just in case

--- modules/noctalia/nixos.nix
+++ modules/noctalia/nixos.nix
@@ -5,6 +5,9 @@
     xdg.configFile."noctalia/settings.json".source =
       config.lib.file.mkOutOfStoreSymlink "${mod}/noctalia/settings.json";
 
+    home.file.".local/state/noctalia/settings.toml".source =
+      config.lib.file.mkOutOfStoreSymlink "${mod}/noctalia/settings.toml";
+
     programs.noctalia-shell.enable = true;
   };
 }
