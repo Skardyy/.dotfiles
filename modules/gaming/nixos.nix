415919f544ab7154f995a25074e97b91bc59f2ca
nixos-meron <meronbssn@gmail.com>
2026-08-29 14:52:38 +0300

    feat(gaming): updating comments

--- modules/gaming/nixos.nix
+++ modules/gaming/nixos.nix
@@ -1,6 +1,10 @@
 { pkgs, user, ... }: {
   #   dont forget to put Proton-GE, and game properties:
-  #   PROTON_ENABLE_WAYLAND=1 PROTON_VKD3D_LOWLATENCY=1 gamemoderun %command%
+  #   MANGOHUD=1 PROTON_ENABLE_WAYLAND=1 WINEDLLOVERRIDES="nvngx_dlssg=d" gamemoderun %command%
+  #
+  #   - proton enable is a must, more performance, better nego
+  #   - dll override is also a must, games might forcefully insert dlssg into the pipeline even when not enabled, only causes issues
+  #   - gamemoderun isn't a must, but couldn't hurt, some games, esp with low fps cap, can have issues with cpu being under util
 
   programs.steam = {
     enable = true;
@@ -11,15 +15,7 @@
     extraCompatPackages = [ pkgs.proton-ge-bin ];
   };
 
-  programs.gamemode = {
-    enable = true;
-    settings = {
-      general = {
-        renice = 10;
-        softrealtime = "auto";
-      };
-    };
-  };
+  programs.gamemode.enable = true;
 
   hardware.graphics = {
     enable = true;
@@ -33,5 +29,17 @@
       discord
       umu-launcher
     ];
+
+    programs.mangohud = {
+      enable = true;
+      settings = {
+        no_display = true;
+        toggle_hud = "F10";
+        fps = true;
+        frame_timing = 1;
+        gpu_stats = true;
+        vram = true;
+      };
+    };
   };
 }
