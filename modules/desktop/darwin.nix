aa4138fc11259a30b27bc3460d78c42b634009ca
meron <meronb@cloud-burst.net>
2026-09-06 13:50:29 +0300

    feat(darwin): disbabling cmd shfit 3/4

--- modules/desktop/darwin.nix
+++ modules/desktop/darwin.nix
@@ -10,6 +10,9 @@
   system.defaults.CustomUserPreferences."com.apple.symbolichotkeys".AppleSymbolicHotKeys = {
     "64" = { enabled = false; }; # Spotlight (cmd+space)
     "65" = { enabled = false; }; # Finder search window (cmd+opt+space)
+    "28" = { enabled = false; }; # Screenshot whole screen to file (cmd+shift+3)
+    "30" = { enabled = false; }; # Screenshot selection to file (cmd+shift+4)
+
     # Screenshot/recording tool: cmd+shift+5 -> cmd+shift+s
     "184" = {
       enabled = true;
