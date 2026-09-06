2ada9529f3b4613a835d6a51916c0d29bc229a0b
meron <meronb@cloud-burst.net>
2026-09-06 10:23:30 +0300

    feat(darwin): changing cmd shift 5 to be cmd shift s

--- modules/desktop/darwin.nix
+++ modules/desktop/darwin.nix
@@ -10,5 +10,13 @@
   system.defaults.CustomUserPreferences."com.apple.symbolichotkeys".AppleSymbolicHotKeys = {
     "64" = { enabled = false; }; # Spotlight (cmd+space)
     "65" = { enabled = false; }; # Finder search window (cmd+opt+space)
+    # Screenshot/recording tool: cmd+shift+5 -> cmd+shift+s
+    "184" = {
+      enabled = true;
+      value = {
+        type = "standard";
+        parameters = [ 115 1 1179648 ];
+      };
+    };
   };
 }
