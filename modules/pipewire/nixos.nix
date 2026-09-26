d4807bc08407ab54cf65eb0ab0659efb3e3f5ecf
nixos-meron <meronbssn@gmail.com>
2026-09-26 14:47:09 +0300

    chore: rev

--- modules/pipewire/nixos.nix
+++ modules/pipewire/nixos.nix
@@ -4,14 +4,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    wireplumber = {
-      enable = true;
-      extraConfig."11-bluetooth-policy" = {
-        "wireplumber.settings" = {
-          "bluetooth.autoswitch-to-headset-profile" = false;
-        };
-      };
-    };
+    wireplumber.enable = true;
   };
   security.rtkit.enable = true;
 }
